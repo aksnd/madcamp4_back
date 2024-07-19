ecc8d9f00d62d288c797cd653beb594d1eecd543
jihun <pjh0490@naver.com>
2024-07-19 03:12:23 +0000

    change into python3 in server

--- madcamp4/settings.py
+++ madcamp4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-s^yznep5rk67yyu@man3t7v!!l*winf&voj_gej92$2kyoa*do
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
